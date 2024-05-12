8bcc5ac89aaa144ce9d286d572311604392c92a1
santinzz <santiagogonuz@gmail.com>
2024-05-11 19:43:27 -0600

    tamaño de letra innesperado

--- src/components/food-item.tsx
+++ src/components/food-item.tsx
@@ -13,7 +13,7 @@ const FoodItem = ({
   return (
     <section className='flex items-center gap-8'>
       <div className='grid gap-4 pt-6'>
-        <Heading className='text-4xl'>{name}</Heading>
+        <Heading className='text-3xl'>{name}</Heading>
         <Paragraph className='max-w-[350px] text-[17px] font-normal'>
           {description}
         </Paragraph>
